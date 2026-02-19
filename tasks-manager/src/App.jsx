1e6f5f7d460c3b8e1672770075a20b84df4ff597
Elisheva <e0583292053@gmail.com>
2026-02-19 21:06:26 +0200

    Creating and basic components

---
+++ tasks-manager/src/App.jsx
@@ -0,0 +1,24 @@
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+
+
+
+
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+
+
+  return (
+    <>
+   
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ tasks-manager/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import { colors } from '@mui/material'
+
+
+
+
+
+        
+
+export default function Home() {
+    return(
+        <>
+        <Box >
+            <Button variant="contained" color='secondary'>let's start!</Button>
+        </Box>
+        </>
+    )
+}
