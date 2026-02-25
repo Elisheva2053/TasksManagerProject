e822d0a1a93edee6c2884556885a24f7c7ebb0f8
Elisheva <e0583292053@gmail.com>
2026-02-25 21:38:10 +0200

    changes in login

--- tasks-manager/src/App.jsx
+++ tasks-manager/src/App.jsx
@@ -3,20 +3,31 @@
 // import '@fontsource/roboto/500.css';
 // import '@fontsource/roboto/700.css';
 
-
-
-
+import { configureStore } from '@reduxjs/toolkit'
+import {provider} from 'react-redux'
 import { useState } from 'react'
+import { Routes, Route, Router } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
+import  Login  from './components/Login';
 
 function App() {
 
+  const myStore=configureStore({
+    reducer:{
+
+    }
+  })
 
   return (
     <>
-   
-      <Home/>
+        <Provider store={myStore}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Counter" element={<Counter />} />
+          </Routes>
+        </Provider>
     </>
   )
 }
--- tasks-manager/src/components/Home.jsx
+++ tasks-manager/src/components/Home.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import { colors } from '@mui/material'
-
-
+import { useState } from 'react'
+import  Login  from './Login'
 
 
 
         
 
 export default function Home() {
+     const [showLogin, setShowLogin] = useState(false)
+
+     const handleLoginButton = () => {
+        setShowLogin(true)
+     }
+    
     return(
         <>
         <Box >
-            <Button variant="contained" color='secondary'>let's start!</Button>
+            <Button variant="contained" color='secondary' onClick={handleLoginButton}>let's start!</Button>
+            {showLogin && <Login />}
         </Box>
         </>
     )
